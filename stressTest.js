17e8be103b1d66292b126b17bde00f0f7b0df116
Fabian Yee <fabianyee952@gmail.com>
2020-12-10 12:27:08 -0800

    change target to 200

--- stressTest.js
+++ stressTest.js
@@ -3,7 +3,7 @@ import { sleep } from 'k6';
 export let options = {
   stages: [
     { duration: '30s', target: 100 },
-    { duration: '1m', target: 500 },
+    { duration: '1m', target: 200 },
     // { duration: '1m30s', target: 1000 },
     // { duration: '2m', target: 5000 },
   ],
