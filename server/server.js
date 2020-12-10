09c805cdbe01625fa4f467c778b6fb7321c0d602
Fabian Yee <fabianyee952@gmail.com>
2020-12-10 12:26:35 -0800

    Stress test

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const router = require('./router');
+require('newrelic');
 
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 const app = express();
---
+++ stressTest.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export let options = {
+  stages: [
+    { duration: '30s', target: 100 },
+    { duration: '1m', target: 500 },
+    // { duration: '1m30s', target: 1000 },
+    // { duration: '2m', target: 5000 },
+  ],
+};
+export default function () {
+  http.get('http://localhost:9898/api/reviews-module/reviews/1233');
+  sleep(1);
+}
